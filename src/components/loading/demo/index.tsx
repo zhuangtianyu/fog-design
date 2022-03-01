11e6b847d87f7392322e363610fb6b80a83d9d71
zhuangtianyu <zhuangtianyu1995@163.com>
2022-03-02 01:00:17 +0800

    feat: update loading component customized icon demo

--- src/components/loading/demo/index.tsx
+++ src/components/loading/demo/index.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import Loading from '@components/loading';
 import Switch from '@components/switch';
+import Icon from '@components/icon';
 import './index.less';
 
 const LoadingDemo = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [tipLoading, setTipLoading] = useState<boolean>(true);
+  const [iconLoading, setIconLoading] = useState<boolean>(true);
 
   return (
     <div className="loading-demo">
@@ -35,6 +37,18 @@ const LoadingDemo = () => {
         </div>
       </Loading>
       <Switch checked={tipLoading} onChange={setTipLoading} />
+      <h3>Customized Icon</h3>
+      <Loading
+        wrapperClassName="loading-container"
+        loading={iconLoading}
+        icon={<Icon type="loading-3/4" />}
+      >
+        <div className="loading-content">
+          <div className="date">2022-03-01</div>
+          <div>Dancing by the sea till night falls.</div>
+        </div>
+      </Loading>
+      <Switch checked={iconLoading} onChange={setIconLoading} />
     </div>
   );
 };
