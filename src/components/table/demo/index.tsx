49cb608ee4962175629bc8e08513950b298300bf
zhuangtianyu <zhuangtianyu1995@163.com>
2022-03-15 23:10:20 +0800

    feat: update table component fixed column

--- src/components/table/demo/index.tsx
+++ src/components/table/demo/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Table, { ColumnType } from '@components/table';
 import './index.less';
 
-const columns: ColumnType[] = [
+const columns1: ColumnType[] = [
   {
     key: 'id',
-    name: 'Name',
+    name: 'Line',
     render: value => `Line ${value}`,
   },
   {
@@ -18,7 +18,7 @@ const columns: ColumnType[] = [
   },
 ];
 
-const data = [
+const data1 = [
   {
     id: 4,
     starting: 'Anheqiao North',
@@ -36,13 +36,74 @@ const data = [
   },
 ];
 
+const columns2: ColumnType[] = [
+  {
+    key: 'id',
+    name: 'No',
+    width: 100,
+    fixed: 'left',
+  },
+  {
+    key: 'name',
+    name: 'Name',
+    width: 150,
+    fixed: 'left',
+  },
+  {
+    key: 'math',
+    name: 'Math',
+  },
+  {
+    key: 'physics',
+    name: 'Physics',
+  },
+  {
+    key: 'chemistry',
+    name: 'Chemistry',
+  },
+  {
+    key: 'total',
+    name: 'Total',
+    fixed: 'right',
+  },
+];
+
+const data2 = [
+  {
+    id: 1001,
+    name: 'Dan',
+    math: 100,
+    physics: 100,
+    chemistry: 100,
+    total: 300,
+  },
+  {
+    id: 1002,
+    name: 'Evan',
+    math: 100,
+    physics: 100,
+    chemistry: 100,
+    total: 300,
+  },
+  {
+    id: 1003,
+    name: 'Johnson',
+    math: 100,
+    physics: 100,
+    chemistry: 100,
+    total: 300,
+  },
+];
+
 const TableDemo = () => {
 
   return (
     <div className="table-demo">
       <h2>Table</h2>
       <h3>Basic Usage</h3>
-      <Table columns={columns} data={data} />
+      <Table columns={columns1} data={data1} />
+      <h3>Fixed Column</h3>
+      <Table columns={columns2} data={data2} contentWidth={1200} />
     </div>
   );
 };
--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useLayoutEffect, useMemo } from 'react';
 import classnames from 'classnames';
 import namespace from '@namespace';
 import useArray from '@hooks/useArray';
@@ -9,6 +9,8 @@ const { prefix } = namespace;
 
 export type ValueType = string | number;
 
+export type FixedType = 'left' | 'right';
+
 export interface RowType {
   [propName: string]: any;
 }
@@ -16,58 +18,161 @@ export interface RowType {
 export interface ColumnType {
   key: string;
   name: string;
+  width?: string | number;
+  fixed?: FixedType;
   render?: (value: ValueType, row: RowType, index: number) => React.ReactNode;
 }
 
-interface TableProps {
+export interface TableProps {
   className?: string;
   data?: RowType[];
   rowKey?: string;
   columns?: ColumnType[];
+  contentWidth?: string | number;
 }
+
+export type ThRefs = Record<string, React.RefObject<HTMLTableCellElement>>;
+
+export type FixedStyles = Record<string, React.CSSProperties>;
+
+const isUsableFixed = fixed => ['left', 'right'].includes(fixed);
 
 const Table: React.FC<TableProps> = props => {
   const {
     className,
     rowKey,
+    contentWidth,
     data: dataFromProps,
     columns: columnsFromProps,
   } = props;
 
-  const columns = useArray<ColumnType[]>(columnsFromProps);
+  const [fixedStyles, setFixedStyles] = useState<FixedStyles>({});
+
+  const columns = useArray<ColumnType[]>(columnsFromProps).map(column => ({
+    ...column,
+    fixed: isUsableFixed(column.fixed) ? column.fixed : undefined,
+  }));
+
   const data = useArray<RowType[]>(dataFromProps);
 
-  return (
-    <table className={classnames(`${prefix}-table`, className)}>
-      <thead>
-        <tr>
-          {columns.map(column => (
-            <th key={column.key}>
-              {column.name}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, index) => (
-          <tr key={rowKey || index}>
-            {columns.map(column => {
-              const { key, render } = column;
+  const thRefs = useMemo<ThRefs>(() =>
+    columns.reduce((accumulator, column) => ({
+      ...accumulator,
+      [column.key]: React.createRef(),
+    }), {})
+  , [columns]);
 
-              return (
-                <td key={key}>
-                  {
-                    isFunction(render)
-                      ? render(row[key], row, index)
-                      : row[key]
-                  }
-                </td>
-              );
-            })}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+  const leftFixedKeys = useMemo(() =>
+    columns
+      .filter(column => column.fixed === 'left')
+      .map(column => column.key)
+  , [columns]);
+
+  const rightFixedKeys = useMemo(() =>
+    columns
+      .filter(column => column.fixed === 'right')
+      .map(column => column.key)
+  , [columns]);
+
+  const isFixedLast = key => leftFixedKeys.length && leftFixedKeys[leftFixedKeys.length - 1] === key;
+
+  const isFixedFirst = key => rightFixedKeys.length && rightFixedKeys[0] === key;
+
+  const updateColumnsLayout = () => {
+    const nextFixedStyles = {};
+
+    let leftOffset = 0;
+    let rightOffset = 0;
+
+    leftFixedKeys.forEach(key => {
+      if (thRefs[key].current) {
+        nextFixedStyles[key] = { left: leftOffset };
+        leftOffset += thRefs[key].current.offsetWidth;
+      }
+    });
+
+    rightFixedKeys.reverse().forEach(key => {
+      if (thRefs[key].current) {
+        nextFixedStyles[key] = { right: rightOffset };
+        rightOffset += thRefs[key].current.offsetWidth;
+      }
+    });
+
+    setFixedStyles(nextFixedStyles);
+  };
+
+  useLayoutEffect(() => {
+    const observers = [];
+
+    Object.keys(thRefs).forEach(key => {
+      const observer = new ResizeObserver(updateColumnsLayout);
+
+      observer.observe(thRefs[key].current);
+      observers.push(observer);
+    });
+
+    return () => observers.forEach(observer => observer.disconnect());
+  }, [thRefs]);
+
+  return (
+    <div className={classnames(`${prefix}-table`, className)}>
+      <div className={`${prefix}-table__content`}>
+        <table width={contentWidth}>
+          <thead className={`${prefix}-table__thead`}>
+            <tr className={`${prefix}-table__tr`}>
+              {columns.map(column => {
+                const { key, fixed } = column;
+
+                return (
+                  <th
+                    className={classnames({
+                      [`${prefix}-table__cell`]: true,
+                      [`${prefix}-table__cell--fixed`]: fixed,
+                      [`${prefix}-table__cell--fixed-last`]: isFixedLast(key),
+                      [`${prefix}-table__cell--fixed-first`]: isFixedFirst(key),
+                    })}
+                    key={key}
+                    ref={thRefs[key]}
+                    style={fixedStyles[key] || {}}
+                  >
+                    {column.name}
+                  </th>
+                );
+              })}
+            </tr>
+          </thead>
+          <tbody className={`${prefix}-table__tbody`}>
+            {data.map((row, index) => (
+              <tr className={`${prefix}-table__tr`} key={rowKey || index}>
+                {columns.map(column => {
+                  const { key, width, fixed, render } = column;
+
+                  return (
+                    <td
+                      className={classnames({
+                        [`${prefix}-table__cell`]: true,
+                        [`${prefix}-table__cell--fixed`]: fixed,
+                        [`${prefix}-table__cell--fixed-last`]: isFixedLast(key),
+                        [`${prefix}-table__cell--fixed-first`]: isFixedFirst(key),
+                      })}
+                      key={key}
+                      width={width}
+                      style={fixedStyles[key] || {}}
+                    >
+                      {
+                        isFunction(render)
+                          ? render(row[key], row, index)
+                          : row[key]
+                      }
+                    </td>
+                  );
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 };
 
