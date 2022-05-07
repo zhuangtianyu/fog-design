08df7d3d86037ccc076baf87117d70649e26e303
zhuangtianyu <zhuangtianyu1995@163.com>
2022-05-08 01:00:47 +0800

    fix: remove grid component col unique key warning

--- src/components/grid/components/row/index.tsx
+++ src/components/grid/components/row/index.tsx
@@ -21,20 +21,20 @@ export const Row: React.FC<RowProps> = props => {
     className,
     style: styleFromProps,
     gutter,
-    children: childrenFromProps,
+    children,
   } = props;
 
-  const children = useMemo(() => {
+  const cols = useMemo(() => {
     const childrenFiltered = [];
 
-    React.Children.forEach(childrenFromProps as React.ReactElement[], item => {
-      if (item.type === Col) {
-        childrenFiltered.push(React.cloneElement(item, { gutter }));
+    React.Children.forEach(children, (item: React.ReactElement) => {
+      if (item && item.type === Col) {
+        childrenFiltered.push(item);
       }
     });
 
-    return childrenFiltered;
-  }, [childrenFromProps, gutter]);
+    return childrenFiltered.map(item => ({ ...item.props, gutter }));
+  }, [children, gutter]);
 
   const style = useMemo(() =>
     typeof gutter === 'number' && gutter > 0
@@ -51,7 +51,11 @@ export const Row: React.FC<RowProps> = props => {
       className={classnames(`${prefix}-row`, className)}
       style={style}
     >
-      {children}
+      {cols.map((item, index) => (
+        <Col key={index} {...item}>
+          {item.children}
+        </Col>
+      ))}
     </div>
   );
 };
