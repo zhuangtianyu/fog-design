d0d234b2ef09d3840d7c81c5ce2e87e265f9965b
zhuangtianyu <zhuangtianyu1995@163.com>
2022-06-13 01:10:23 +0800

    feat: update slider mouse events binding

--- src/components/slider/index.tsx
+++ src/components/slider/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState, useEffect, useRef } from 'react';
 import classnames from 'classnames';
 import namespace from '@namespace';
 import useControlled from '@hooks/useControlled';
@@ -22,21 +23,79 @@ export const Slider: React.FC<SliderProps> = props => {
     onChange: onChangeFromProps,
   } = props;
 
-  const { value } = useControlled({
+  const { value, onChange } = useControlled({
     value: valueFromProps,
     defaultValue: defaultValueFromProps,
     onChange: onChangeFromProps,
   });
 
-  const offset = `${value * 100}%`;
+  const trackRef = useRef<HTMLDivElement>(null);
 
-  console.log(offset);
+  const trackOffsetX = useRef<number>(0);
+
+  const [dragging, setDragging] = useState<boolean>(false);
+
+  const [dragValue, setDragValue] = useState<number>(0);
+
+  const left = `${dragValue * 100}%`;
+
+  useEffect(() => {
+    setDragValue(value);
+  }, [value]);
+
+  const handleDragStart = (event: React.MouseEvent<HTMLDivElement>) => {
+    setDragging(true);
+
+    trackOffsetX.current = event.clientX - event.currentTarget.offsetLeft;
+  };
+
+  useEffect(() => {
+    if (dragging) {
+      const handleDrag = (event: MouseEvent) => {
+        const diff = event.clientX - trackOffsetX.current;
+        const nextDragValue = diff / trackRef.current.offsetWidth;
+
+        if (nextDragValue >= 0 && nextDragValue <= 1) {
+          setDragValue(nextDragValue);
+        }
+      };
+
+      window.addEventListener('mousemove', handleDrag);
+
+      return () => window.removeEventListener('mousemove', handleDrag);
+    }
+  }, [dragging]);
+
+  useEffect(() => {
+    if (dragging) {
+      const handleDragEnd = () => {
+        dragValue !== value && onChange(dragValue);
+
+        setDragging(false);
+      };
+
+      window.addEventListener('mouseup', handleDragEnd);
+
+      return () => window.removeEventListener('mouseup', handleDragEnd);
+    }
+  }, [dragging, dragValue, value]);
 
   return (
     <div className={classnames(`${prefix}-slider`, className)}>
-      <div className={`${prefix}-slider__track`}>
-        <div className={`${prefix}-slider__passed`} style={{ width: offset }} />
-        <div className={`${prefix}-slider__handle`} style={{ left: offset }} />
+      <div
+        className={`${prefix}-slider__track`}
+        ref={trackRef}
+      >
+        <div
+          className={`${prefix}-slider__passed`}
+          style={{ width: left }}
+        />
+        <div
+          className={`${prefix}-slider__handle`}
+          style={{ left }}
+          draggable={false}
+          onMouseDown={handleDragStart}
+        />
       </div>
     </div>
   );
