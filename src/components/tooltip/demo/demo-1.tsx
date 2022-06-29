5c82cb5a422a7f961c2bce10be1c12aeff85391e
zhuangtianyu <zhuangtianyu1995@163.com>
2022-06-30 02:00:09 +0800

    feat: extend tooltip children types

--- src/components/tooltip/demo/demo-1.tsx
+++ src/components/tooltip/demo/demo-1.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 import Tooltip from '@components/tooltip';
 
 const Demo = () => (
-  <Tooltip content="Tooltip content.">
-    <span>Hover me</span>
-  </Tooltip>
+  <>
+    <Tooltip content="Tooltip content.">
+      Hover me
+    </Tooltip>
+    <Tooltip content="Tooltip content.">
+      <span style={{ marginLeft: 12, marginRight: 12 }}>
+        Hover me
+      </span>
+    </Tooltip>
+    <Tooltip content="Tooltip content.">
+      <>Hover me</>
+    </Tooltip>
+  </>
 );
 
 export default Demo;
--- src/components/tooltip/index.tsx
+++ src/components/tooltip/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useMemo } from 'react';
 import classnames from 'classnames';
 import namespace from '@namespace';
 import Trigger, { TriggerProps } from '@components/trigger/index';
@@ -6,9 +7,9 @@ import './index.less';
 
 const { prefix } = namespace;
 
-export interface TooltipProps extends TriggerProps  {
+export interface TooltipProps extends Omit<TriggerProps, 'children'>  {
   popupClassName?: string;
-  children?: React.ReactElement;
+  children?: React.ReactChild | React.ReactChild[];
   content?: React.ReactChild;
 }
 
@@ -20,13 +21,32 @@ export const Tooltip: React.FC<TooltipProps> = props => {
     ...restProps
   } = props;
 
+  const wrappedChildren = useMemo(() => {
+    const childrenIsArray = Array.isArray(children);
+    const childrenIsFragment = !childrenIsArray && (children as React.ReactElement).type === React.Fragment;
+
+    const shouldWrap =
+      childrenIsArray ||
+      childrenIsFragment ||
+      typeof children === 'string' ||
+      typeof children === 'number';
+
+    if (shouldWrap) return (
+      <span className={`${prefix}-tooltip__trigger`}>
+        {children}
+      </span>
+    );
+
+    return children;
+  }, [children]);
+
   return (
     <Trigger
       popupClassName={classnames(`${prefix}-tooltip`, popupClassName)}
       popup={<>{content}</>}
       {...restProps}
     >
-      {children}
+      {wrappedChildren}
     </Trigger>
   );
 };
