3f13399c9b643f02ff36236490d7bb6520d839f1
zhuangtianyu <zhuangtianyu1995@163.com>
2022-07-23 01:00:07 +0800

    feat: add getFormItemValueEmpty to form adapter

--- src/components/form/adapter.ts
+++ src/components/form/adapter.ts
@@ -2,13 +2,14 @@ import Radio from '@components/radio';
 import Checkbox from '@components/checkbox';
 import Input from '@components/input';
 import InputNumber from '@components/input-number';
-import Select from '@components/select';
 import Switch from '@components/switch';
+import Select, { SelectProps } from '@components/select';
 import DatePicker from '@components/date-picker';
+import React from 'react';
 
 type GetFormItemValueGetter = (element: React.ReactElement) => (...args: any) => any;
 
-export const getFormItemValueGetter: GetFormItemValueGetter = (element: React.ReactElement) => {
+export const getFormItemValueGetter: GetFormItemValueGetter = element => {
   switch (element.type) {
     case Input:
     case InputNumber:
@@ -25,5 +26,32 @@ export const getFormItemValueGetter: GetFormItemValueGetter = (element: React.Re
       return nextValue => nextValue;
     default:
       return null;
+  }
+};
+
+type GetFormItemValueEmpty = (element: React.ReactElement, value: any) => boolean;
+
+export const getFormItemValueEmpty: GetFormItemValueEmpty = (element, value) => {
+  const empties = [null, undefined];
+
+  switch (element.type) {
+    case Radio:
+    case Switch:
+    case Checkbox:
+    case DatePicker:
+    case InputNumber:
+    case Radio.Group:
+    case Checkbox.Group:
+      return empties.includes(value);
+    case Input:
+      return !value;
+    case Select:
+      return !(element.props as SelectProps).multiple
+        ? empties.includes(value)
+        : value.length;
+    case DatePicker.RangePicker:
+      return value.length;
+    default:
+      return true;
   }
 };
